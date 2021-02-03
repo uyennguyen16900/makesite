7e21e642b26cd0cf0ab3338ef14e77c01cd32f3a
uyennguyen16900 <uyennguyen16900@gmail.com>
2021-02-02 16:40:48 -0800

    rendered file content and wrote html template

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module makesite
 
 go 1.13
+
+require golang.org/x/tools/gopls v0.6.4 // indirect
--- makesite.go
+++ makesite.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+	"html/template"
+)
+
+// Data is 
+type Data struct {
+	Content string 
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFile(filename string) string {
+	fileContent, err := ioutil.ReadFile(filename)
+	check(err)
+	return string(fileContent)
+}
+
+
+func writeTemplate(fileContent string, templateFile string, newFile string) {
+	tmpl := template.Must(template.New(templateFile).ParseFiles(templateFile))
+	content := Data{Content: string(fileContent)}
+	
+	f, err := os.Create(newFile)
+	check(err)
+	defer f.Close()
+
+	err = tmpl.Execute(os.Stdout, content)
+	check(err)
+
+	f.Close()
+}
+
 
 func main() {
-	fmt.Println("Hello, world!")
+	writeTemplate(readFile("first-post.txt"), "template.tmpl", "first-post.html")
+	
+	
+	
 }
