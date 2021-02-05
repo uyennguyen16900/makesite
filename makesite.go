eef78885e693ac47e7419fedde0741f5ad304e2d
uyennguyen16900 <uyennguyen16900@gmail.com>
2021-02-04 17:40:16 -0800

    Added a flag to turn a given text file into a html file

--- makesite.go
+++ makesite.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 // Data is
@@ -23,27 +25,27 @@ func readFile(filename string) string {
 	return string(fileContent)
 }
 
-func renderTemplate(fileContent string, templateFile string, newFile string) {
+func renderTemplate(fileContent string, templateFile string, file string) {
 	tmpl := template.Must(template.New(templateFile).ParseFiles(templateFile))
 	content := Data{Content: string(fileContent)}
 
-	f, err := os.Create(newFile)
+	filename := strings.Split(file, ".txt")[0]
+	f, err := os.Create(filename + ".html")
 	check(err)
 	defer f.Close()
 
 	err = tmpl.Execute(f, content)
 	check(err)
-
-	f.Close()
 }
 
 func main() {
-	renderTemplate(readFile("first-post.txt"), "template.tmpl", "first-post.html")
-
-	filePtr := flag.StringVar(...., "file", "defaultValue", "Text file name to turn to HTML page.")
+	var fileName string
+	flag.StringVar(&fileName, "file", "defaultValue", "Text file name to turn to HTML page.")
 	flag.Parse()
-	save(filePtr)
-	
-	
-	fmt.Println("file:", *filePtr)
+	// save(filePtr)
+
+	if fileName == "" {
+		panic("Missing file name!")
+	}
+	renderTemplate(readFile(fileName), "template.tmpl", fileName)
 }
